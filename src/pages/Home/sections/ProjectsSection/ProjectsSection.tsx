817b0a27c681ede696bdec48996269ecac4e95c5
Vitor Rosa <vitor.rosa.dev@outlook.com>
2025-07-28 18:29:51 -0300

    feat: change image format

--- src/pages/Home/sections/ProjectsSection/ProjectsSection.tsx
+++ src/pages/Home/sections/ProjectsSection/ProjectsSection.tsx
@@ -13,7 +13,7 @@ const ProjectsSection: React.FC = () => {
         {
             title: "App Expert Oppo",
             subtitle: "Dez 2024 - Atual",
-            srcImg: "/src/assets/images/smartphone.png",
+            srcImg: "/src/assets/images/smartphone.jpg",
             /* description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porta semper velit vel rutrum. Aliquam vulputate, nisi eget tristique mattis, nisi sem faucibus eros, a auctor felis sem ut mauris. Phasellus a ultrices elit. Curabitur ut diam eu orci auctor pretium.", */
             technologies: "Technologies: Ionic, Angular, Capacitor, HTML, CSS",
             websiteURL: "https://onedrive.live.com/?id=5AA5356BDE13B7C5%21s85e8c38dfdf54cc986e6e2c5857b14f8&cid=5AA5356BDE13B7C5&redeem=aHR0cHM6Ly8xZHJ2Lm1zL2YvYy81YWE1MzU2YmRlMTNiN2M1L0VvM0Q2SVgxX2NsTWh1Yml4WVY3RlBnQkpOQzVaVm9NYW1TdVpOaWRwNmFiYmc%5FZT1Na1hORkY",
@@ -22,7 +22,7 @@ const ProjectsSection: React.FC = () => {
         {
             title: "Turismo Alvorada",
             subtitle: "Ago 2024",
-            srcImg: "/src/assets/images/turismo.png",
+            srcImg: "/src/assets/images/turismo.jpg",
             /* description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porta semper velit vel rutrum. Aliquam vulputate, nisi eget tristique mattis, nisi sem faucibus eros, a auctor felis sem ut mauris. Phasellus a ultrices elit. Curabitur ut diam eu orci auctor pretium.", */
             technologies: "Technologies: Ionic, Angular, Capacitor, HTML, CSS",
             websiteURL: "https://github.com/VitorRosaDev/passeio-alvorada-app",
@@ -31,7 +31,7 @@ const ProjectsSection: React.FC = () => {
         {
             title: "Carreira Dev",
             subtitle: "Set 2024",
-            srcImg: "/src/assets/images/chapeu.png",
+            srcImg: "/src/assets/images/chapeu.jpg",
             /* description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porta semper velit vel rutrum. Aliquam vulputate, nisi eget tristique mattis, nisi sem faucibus eros, a auctor felis sem ut mauris. Phasellus a ultrices elit. Curabitur ut diam eu orci auctor pretium.", */
             technologies: "Technologies: JavaScript, HTML, CSS",
             websiteURL: "https://vitorrosadev.github.io/imersao-alura-gemini/",
@@ -40,7 +40,7 @@ const ProjectsSection: React.FC = () => {
         {
             title: "Cordel Moderno",
             subtitle: "Ago 2024",
-            srcImg: "/src/assets/images/logo-leitura.png",
+            srcImg: "/src/assets/images/logo-leitura.jpg",
             /* description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porta semper velit vel rutrum. Aliquam vulputate, nisi eget tristique mattis, nisi sem faucibus eros, a auctor felis sem ut mauris. Phasellus a ultrices elit. Curabitur ut diam eu orci auctor pretium.", */
             technologies: "Technologies: JavaScript, HTML, CSS",
             websiteURL: "https://vitorrosadev.github.io/projeto-cordel/",
@@ -49,7 +49,7 @@ const ProjectsSection: React.FC = () => {
         {
             title: "X-Devs",
             subtitle: "Jun 2024",
-            srcImg: "/src/assets/images/logo-xdevs.png",
+            srcImg: "/src/assets/images/logo-xdevs.jpg",
             /* description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porta semper velit vel rutrum. Aliquam vulputate, nisi eget tristique mattis, nisi sem faucibus eros, a auctor felis sem ut mauris. Phasellus a ultrices elit. Curabitur ut diam eu orci auctor pretium.", */
             technologies: "Technologies: JavaScript, HTML, CSS",
             websiteURL: "https://vitorrosadev.github.io/x-devs-project/",
@@ -58,7 +58,7 @@ const ProjectsSection: React.FC = () => {
         {
             title: "Dragões",
             subtitle: "Ago 2024",
-            srcImg: "/src/assets/images/logo-dragao.png",
+            srcImg: "/src/assets/images/logo-dragao.jpg",
             /* description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porta semper velit vel rutrum. Aliquam vulputate, nisi eget tristique mattis, nisi sem faucibus eros, a auctor felis sem ut mauris. Phasellus a ultrices elit. Curabitur ut diam eu orci auctor pretium.", */
             technologies: "Technologies: JavaScript, HTML, CSS",
             websiteURL: "https://vitorrosadev.github.io/casa-do-dragao/",
@@ -67,7 +67,7 @@ const ProjectsSection: React.FC = () => {
         {
             title: "Android",
             subtitle: "Ago 2024",
-            srcImg: "/src/assets/images/logo-android.png",
+            srcImg: "/src/assets/images/logo-android.jpg",
             /* description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porta semper velit vel rutrum. Aliquam vulputate, nisi eget tristique mattis, nisi sem faucibus eros, a auctor felis sem ut mauris. Phasellus a ultrices elit. Curabitur ut diam eu orci auctor pretium.", */
             technologies: "Technologies: JavaScript, HTML, CSS",
             websiteURL: "https://vitorrosadev.github.io/exercicio-android/",
